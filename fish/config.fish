750aafb666c9888c244f038abfc3a19088b3dcf6
Korbin de Man <korbindeman4@gmail.com>
2024-03-27 02:14:13 +0100

    use fisher
    fish plugins sponge and tide added
    
    use fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,7 @@
 fish_vi_key_bindings
 
+set fish_greeting
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export XDG_CONFIG_HOME="$HOME/.config"
-
-starship init fish | source
-zoxide init --cmd cd fish | source
