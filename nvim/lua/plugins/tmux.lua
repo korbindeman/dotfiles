9f85803b6a762b854f4061ef71adb94943a6fbdd
Korbin de Man <korbindeman4@gmail.com>
2024-03-29 01:41:36 +0100

    switch to Navigator.nvim instead of tmux.nvim (to fix yank)

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,4 +1,22 @@
 return {
-	"aserowy/tmux.nvim",
-	config = true,
+	"numToStr/Navigator.nvim",
+	keys = {
+		{
+			"<C-h",
+			"<CMD>NavigatorLeft<CR>",
+		},
+		{
+			"<C-l>",
+			"<CMD>NavigatorRight<CR>",
+		},
+		{
+			"<C-k>",
+			"<CMD>NavigatorUp<CR>",
+		},
+		{
+			"<C-j>",
+			"<CMD>NavigatorDown<CR>",
+		},
+	},
+	opts = {},
 }
