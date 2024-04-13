b1348f6af4fec77f07d3cb10bf86548a12bd6fac
Korbin de Man <korbindeman4@gmail.com>
2024-04-13 16:58:50 +0200

    improve incline config

--- nvim/lua/plugins/incline.lua
+++ nvim/lua/plugins/incline.lua
@@ -1,25 +1,19 @@
 return {
 	"b0o/incline.nvim",
 	config = function()
-		local helpers = require("incline.helpers")
 		local devicons = require("nvim-web-devicons")
 
 		require("incline").setup({
-			window = {
-				padding = 1,
-				margin = { horizontal = 0 },
-			},
 			render = function(props)
 				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
 				if filename == "" then
-					filename = "[No Name]"
+					filename = "no file"
 				end
-				local ft_icon, ft_color = devicons.get_icon_color(filename)
-				local modified = vim.bo[props.buf].modified
+				local ft_icon, ft_color = devicons.get_icon_color(filename, nil, { default = true })
 				return {
-					ft_icon,
+					{ ft_icon, guifg = ft_color },
 					" ",
-					{ filename, gui = modified and "bold,italic" or "bold" },
+					filename,
 				}
 			end,
 		})
